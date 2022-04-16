2b15cb9940df92d4631a1a88d7481013394ee1de
nvloc0411 <69301479+nvloc0411@users.noreply.github.com>
2022-04-16 10:51:24 +0700

    Create band-name-generator.py

---
+++ band-name-generator.py
@@ -0,0 +1,10 @@
+#1. Create a greeting for your program.
+print('Hello')
+#2. Ask the user for the city that they grew up in.
+user_city = input('Bạn ở đâu?\n')
+#3. Ask the user for the name of a pet.
+user_pet = input('Tên thú cưng của bạn là gì?\n')
+#4. Combine the name of their city and pet and show them their band name.
+print("Tên nhóm nhạc của bạn là: " + user_city + user_pet)
+#5. Make sure the input cursor shows on a new line, see the example at:
+#   https://replit.com/@appbrewery/band-name-generator-end
